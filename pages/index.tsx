bde99c9d5bdc0a75df784c9295a6dc6072b5bc1a
eastasian <junceliz@gmail.com>
2022-01-08 17:55:34 +0900

    edit convert project to typescript

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="kabchi" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>kabchi</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home
