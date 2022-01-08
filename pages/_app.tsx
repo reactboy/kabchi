2b0261c74a71dd7060334c8046b156269ba8077d
eastasian <junceliz@gmail.com>
2022-01-08 18:12:47 +0900

    edit configure chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 import { AppProps } from "next/app"
+import { ChakraProvider } from "@chakra-ui/provider"
+import { extendTheme } from "@chakra-ui/react"
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+const colors = {
+
 }
 
-export default MyApp
+const fonts = {
+
+}
+
+const theme = extendTheme({ colors, fonts })
+
+const Kabchi = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default Kabchi
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import Head from 'next/head'
+import { Heading, Box } from '@chakra-ui/react'
 
 const Home = () => {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>kabchi</title>
         <meta name="description" content="kabchi" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <h1>kabchi</h1>
-      </div>
+      <Box>
+        <Heading as="h1">kabchi</Heading>
+      </Box>
     </>
   )
 }
