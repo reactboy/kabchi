5f462a0e157dadb5417caa4b6ca0014adc64eb25
eastasian <junceliz@gmail.com>
2022-01-14 23:45:48 +0900

    add create base for Button components #10

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,11 @@
+export const Button = () => {
+  return <>button</>
+}
+
+export const GuestSignInButton = () => {
+  return <>sign in as a guest</>
+}
+
+export const GoogleSignInButton = () => {
+  return <>Sign in with google</>
+}
