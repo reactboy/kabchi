341366562457d0034ee8d1cdb723a62af0096465
eastasian <junceliz@gmail.com>
2022-01-14 23:31:09 +0900

    edit modified AppLayout and index page #8

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,14 @@
-import Head from 'next/head'
-import { Heading } from '@chakra-ui/react'
-
 import { AppLayout } from 'components/layout'
 
 const Home = () => {
   return (
-    <>
-      <Head>
-        <title>kabchi</title>
-        <meta name="description" content="kabchi" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <AppLayout>
-        <Heading as="h1" color="kbpurple.900">
-          kabchi
-        </Heading>
-      </AppLayout>
-    </>
+    <AppLayout>
+      <div>
+        <p>slowly but surely</p>
+        <p>achieve your goal</p>
+        <p>with kabchi</p>
+      </div>
+    </AppLayout>
   )
 }
 
