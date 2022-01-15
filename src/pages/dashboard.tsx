c0e812c5541caed5f36cd0631228dd6a7be7c812
eastasian <junceliz@gmail.com>
2022-01-15 17:57:32 +0900

    add scaffold dashboard and walls pages #5

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+import { AppLayout } from 'components/layout'
+
+const Dashboard: NextPage = () => {
+  return <AppLayout>Dash board</AppLayout>
+}
+
+export default Dashboard
