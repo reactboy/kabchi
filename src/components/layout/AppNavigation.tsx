a6e00b22b4dfdb5d15e52671b0d18eb3520af34d
eastasian <junceliz@gmail.com>
2022-01-15 18:12:28 +0900

    add create base file for app navigation component #5

---
+++ src/components/layout/AppNavigation.tsx
@@ -0,0 +1,11 @@
+import { Flex } from '@chakra-ui/react'
+
+import { Logo } from 'components/common'
+
+export const AppNavigation = () => {
+  return (
+    <Flex>
+      <Logo logoType="icon" />
+    </Flex>
+  )
+}
