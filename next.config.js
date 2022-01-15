8765de07a18a184c6aa36b5453b8c2dc7841d54e
eastasian <junceliz@gmail.com>
2022-01-15 17:58:49 +0900

    edit configure page redirect to /dashboard on accessing /walls #5

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/walls',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ]
+  },
 }
