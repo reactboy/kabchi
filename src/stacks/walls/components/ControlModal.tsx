5573df9962e86e874693e79a70320726126edd45
eastasian <junceliz@gmail.com>
2022-01-29 19:37:01 +0900

    edit implement form validation with formik for walls create and edit #35

--- src/stacks/walls/components/ControlModal.tsx
+++ src/stacks/walls/components/ControlModal.tsx
@@ -1,4 +1,4 @@
-import { VFC, FormEventHandler, ChangeEvent } from 'react'
+import { VFC } from 'react'
 import {
   UseDisclosureReturn,
   FormControl,
@@ -8,6 +8,7 @@ import {
   HStack,
   VStack,
 } from '@chakra-ui/react'
+import { Formik } from 'formik'
 
 import {
   Modal,
@@ -18,59 +19,73 @@ import {
   Button,
 } from 'components/common'
 
-import { store } from 'redux/app'
-import { setWallInput, selectWallInput } from 'redux/feature'
+import { selectWallInput, WallInput } from 'redux/feature'
 
 type ControlModalProps = {
   isOpen: UseDisclosureReturn['isOpen']
   onClose: UseDisclosureReturn['onClose']
-  onSubmit: FormEventHandler
+  onSubmit: (values: WallInput) => Promise<void>
   submitText?: string
+}
+
+const validate = (values) => {
+  const errors: { title?: string; description?: string } = {}
+
+  if (!values.title) errors.title = 'Required'
+  if (!values.description) errors.description = 'Required'
+
+  return errors
 }
 
 export const ControlModal: VFC<ControlModalProps> = (props) => {
   const { isOpen, onClose, onSubmit, submitText = 'submit' } = props
   const wallInput = selectWallInput()
 
-  const onChangeHandler =
-    (key: keyof typeof wallInput) =>
-    (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-      store.dispatch(setWallInput({ ...wallInput, [key]: e.target.value }))
-    }
-
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
-      <ModalContent as="form" onSubmit={onSubmit}>
-        <ModalBody as={VStack}>
-          <FormControl>
-            <FormLabel fontSize={14} color="kbpurple.900" fontWeight="bold">
-              Title
-            </FormLabel>
-            <Input
-              variant="filled"
-              placeholder="describe your goal in short."
-              value={wallInput.title}
-              onChange={onChangeHandler('title')}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel fontSize={14} color="kbpurple.900" fontWeight="bold">
-              Description
-            </FormLabel>
-            <Textarea
-              variant="filled"
-              placeholder="brief description of goal."
-              value={wallInput.description}
-              onChange={onChangeHandler('description')}
-            />
-          </FormControl>
-        </ModalBody>
-        <ModalFooter as={HStack}>
-          <Button onClick={onClose}>close</Button>
-          <Button type="submit">{submitText}</Button>
-        </ModalFooter>
-      </ModalContent>
+      <Formik
+        initialValues={{ ...wallInput }}
+        onSubmit={onSubmit}
+        validate={validate}
+      >
+        {(formik) => (
+          <ModalContent as="form" onSubmit={formik.handleSubmit}>
+            <ModalBody as={VStack}>
+              <FormControl>
+                <FormLabel fontSize={14} color="kbpurple.900" fontWeight="bold">
+                  Title
+                </FormLabel>
+                <Input
+                  variant="filled"
+                  placeholder="describe your goal in short."
+                  name="title"
+                  onChange={formik.handleChange}
+                  value={formik.values.title}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel fontSize={14} color="kbpurple.900" fontWeight="bold">
+                  Description
+                </FormLabel>
+                <Textarea
+                  variant="filled"
+                  placeholder="brief description of goal."
+                  name="description"
+                  onChange={formik.handleChange}
+                  value={formik.values.description}
+                />
+              </FormControl>
+            </ModalBody>
+            <ModalFooter as={HStack}>
+              <Button onClick={onClose}>close</Button>
+              <Button disabled={!formik.dirty || !formik.isValid} type="submit">
+                {submitText}
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        )}
+      </Formik>
     </Modal>
   )
 }
