ec328fb588441ce4eb7d03e8829de6e50728096f
eastasian <junceliz@gmail.com>
2022-02-10 13:17:22 +0900

    edit modified about page design #86

--- src/stacks/about/components/DescriptionBlock.tsx
+++ src/stacks/about/components/DescriptionBlock.tsx
@@ -4,13 +4,5 @@ import { Box, BoxProps } from '@chakra-ui/react'
 type DescriptionBlockProps = BoxProps
 
 export const DescriptionBlock: VFC<DescriptionBlockProps> = (props) => {
-  return (
-    <Box
-      p="2"
-      border="1px solid"
-      borderColor="kbgray.900"
-      borderRadius={18}
-      {...props}
-    />
-  )
+  return <Box p="2" {...props} />
 }
